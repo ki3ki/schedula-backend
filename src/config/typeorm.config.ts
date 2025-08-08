667b939f7cc66e8796c156d4bbe3152fcbeec5ab
Keerthi <keerthypkumar11@gmail.com>
2025-08-08 13:25:02 +0530

    Fix: use environment variables in TypeORM config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '12345',
-  database: 'schedula',
+  host: process.env.DB_HOST!, // Non-null assertion
+  port: parseInt(process.env.DB_PORT!, 10), // Non-null assertion
+  username: process.env.DB_USERNAME!,
+  password: process.env.DB_PASSWORD!,
+  database: process.env.DB_NAME!,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false,
-  migrations: ['dist/migrations/*.js']
+  migrations: ['dist/migrations/*.js'],
 };
